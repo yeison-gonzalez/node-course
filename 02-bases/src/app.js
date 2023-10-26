a9e40d8e61295201e69524fae086b7f406107282
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-10-26 15:56:44 -0500

    [ADD] first class

---
+++ 02-bases/src/app.js
@@ -0,0 +1,2 @@
+const { emailTemplate } = require('./js-foundation/01-template')
+console.log(emailTemplate)
---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,8 @@
+const emailTemplate = `
+    <div>
+        <h1>Hi, {{name}}</h1>
+        <p>Thank you for your order.</p>
+    </div>
+`;
+
+module.exports = { emailTemplate }
