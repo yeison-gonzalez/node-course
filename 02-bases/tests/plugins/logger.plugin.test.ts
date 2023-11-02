0dd6e5f15b8937e7f4eaf275181fc1d676a9e20b
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-11-02 15:36:16 -0500

    [ADD] finalize testing

---
+++ 02-bases/tests/plugins/logger.plugin.test.ts
@@ -0,0 +1,45 @@
+import { buildLogger, logger as winstonLogger } from '../../src/plugins/logger.plugin'
+
+describe('plugins/logger.plugin.ts', () => {
+  it('buildLogger should return a function logger', () => {
+    const logger = buildLogger('test')
+    expect(typeof logger.log).toBe('function')
+    expect(typeof logger.error).toBe('function')
+  })
+
+  it('logger.log should log a message', () => {
+    const winstonLoggerMock = jest.spyOn(winstonLogger, 'log')
+    const message = 'test message'
+    const service = 'test service'
+
+    const logger = buildLogger(service)
+
+    logger.log(message)
+
+    expect(winstonLoggerMock).toHaveBeenCalledWith(
+      'info',
+      expect.objectContaining({
+        level: 'info',
+        message,
+        service,
+      }));
+  })
+
+  it('logger.error should return a error message', () => {
+    const winstonLoggerMock = jest.spyOn(winstonLogger, 'error')
+    const message = 'test message error'
+    const service = 'test service'
+
+    const logger = buildLogger(service)
+
+    logger.error(message)
+
+    expect(winstonLoggerMock).toHaveBeenCalledWith(
+      'error',
+      expect.objectContaining({
+        message,
+        service
+      })
+    );
+  })
+})
