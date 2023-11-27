b3fdfc947c2f10d0e2980341f103eb710e088ea7
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-11-27 15:12:19 -0500

    [ADD] create dto for create todo

--- 07-rest-web/src/presentation/todos/controller.ts
+++ 07-rest-web/src/presentation/todos/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
+import { CreateTodoDto } from "../../domain/dtos";
 
 export class TodosController {
   constructor() { }
@@ -24,12 +25,12 @@ export class TodosController {
   }
 
   public createTodo = async (req: Request, res: Response) => {
-    const { text } = req.body;
+    const [error, createTodoDto] = CreateTodoDto.create(req.body);
 
-    if (!text) return res.status(400).json({ error: 'Text property is required' });
+    if (error) return res.status(400).json({ error });
 
     const todo = await prisma.todo.create({
-      data: { text, completedAt: new Date() }
+      data: createTodoDto!
     });
 
     return res.json(todo);
