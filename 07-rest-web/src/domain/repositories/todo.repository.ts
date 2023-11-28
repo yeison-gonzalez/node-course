17de9c6b8e6b311ab2ae684b2cff2d7f7fe0d01d
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-11-28 14:16:52 -0500

    [ADD] create todo datasource and respository

---
+++ 07-rest-web/src/domain/repositories/todo.repository.ts
@@ -0,0 +1,14 @@
+import { CreateTodoDto, UpdateTodoDto } from "../dtos";
+import { TodoEntity } from "../entities/todo.entity";
+
+export abstract class TodoRepository {
+  abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;
+
+  abstract getAll(): Promise<TodoEntity[]>;
+
+  abstract findById(id: number): Promise<TodoEntity>;
+
+  abstract updateById(updatedTodoDto: UpdateTodoDto): Promise<TodoEntity>;
+
+  abstract deleteById(id: number): Promise<TodoEntity>
+}
