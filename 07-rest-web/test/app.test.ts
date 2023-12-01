aaa2b8e7334e252c2b8844a88032c1f828ced5be
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-12-01 15:14:06 -0500

    [ADD] add testing server

--- 07-rest-web/test/app.test.ts
+++ 07-rest-web/test/app.test.ts
@@ -1,5 +1,16 @@
-describe('Testing App.ts', () => {
-  test('should work', () => {
-    expect(true).toBeTruthy();
+import { envs } from "../src/config/envs";
+import { Server } from "../src/presentation/server";
+
+jest.mock('../src/presentation/server')
+
+describe('should call server with arguments and start', () => {
+  test('should work', async () => {
+    await import('../src/app');
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      routes: expect.any(Function),
+    })
+    expect(Server.prototype.start).toHaveBeenCalledWith();
   })
 })
