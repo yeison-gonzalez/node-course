ce7bedfaca52844a099ddb8093aa2d4c136e107c
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-12-04 14:19:43 -0500

    [ADD] add create todo testing

--- 07-rest-web/jest.config.ts
+++ 07-rest-web/jest.config.ts
@@ -192,7 +192,9 @@ const config: Config = {
   // verbose: undefined,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  // watchPathIgnorePatterns: [],
+  watchPathIgnorePatterns: [
+    "<rootDir>/jest.config.ts"
+  ],
 
   // Whether to use watchman for file crawling
   // watchman: true,
--- 07-rest-web/test/presentation/todos/routes.test.ts
+++ 07-rest-web/test/presentation/todos/routes.test.ts
@@ -56,4 +56,35 @@ describe('Todo route testing', () => {
 
     expect(body).toEqual({ error: `Todo with id: ${todoId} not found` })
   });
+
+  test('should return a new Todo api/todos', async () => {
+    const { body } = await request(testServer.app)
+      .post('/api/todos')
+      .send(todo1)
+      .expect(200);
+    
+    expect(body).toEqual({
+      id: expect.any(Number),
+      text: todo1.text,
+      completedAt: null,
+    });
+  });
+
+  test('should return an error if text is present api/todos', async () => {
+    const { body } = await request(testServer.app)
+      .post('/api/todos')
+      .send({ })
+      .expect(400);
+    
+    expect(body).toEqual({ error: 'Text property is required' });
+  });
+
+  test('should return an error if text is empty api/todos', async () => {
+    const { body } = await request(testServer.app)
+      .post('/api/todos')
+      .send({ text: '' })
+      .expect(400);
+    
+    expect(body).toEqual({ error: 'Text property is required' });
+  });
 });
