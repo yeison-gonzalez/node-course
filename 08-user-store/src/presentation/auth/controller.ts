acf39aa715f6e7b31f515340ba93fd0855a0056e
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-12-06 15:03:04 -0500

    [ADD] create login auth service

--- 08-user-store/src/presentation/auth/controller.ts
+++ 08-user-store/src/presentation/auth/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { CustomError, RegisterUserDto } from '../../domain';
 import { AuthService } from '../services/auth.service';
+import { LoginUserDto } from '../../domain/dtos/auth/login-user.dto';
 
 export class AuthController {
   constructor(
@@ -16,14 +17,18 @@ export class AuthController {
   
   register = (req: Request, res: Response) => {
     const [error, registerDto] = RegisterUserDto.create(req.body);
-    if (error) return res.status(400).json({ error })
+    if (error) return res.status(400).json({ error });
     this.authService.registerUser(registerDto!)
       .then((user) => res.json(user))
       .catch(error => this.handleError(error, res));
   }
 
   loginUser = (req: Request, res: Response) => {
-    res.json('registerUser');
+    const [error, loginUserDto] = LoginUserDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+    this.authService.loginUser(loginUserDto!)
+      .then(user => res.json(user))
+      .catch(error => this.handleError(error, res));
   }
 
   validateEmail = (req: Request, res: Response) => {
--- 08-user-store/src/presentation/services/auth.service.ts
+++ 08-user-store/src/presentation/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { bcryptAdapter } from "../../config";
 import { UserModel } from "../../data";
-import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { CustomError, LoginUserDto, RegisterUserDto, UserEntity } from "../../domain";
 
 export class AuthService {
   constructor() { }
@@ -15,11 +15,28 @@ export class AuthService {
       await user.save();
       const { password, ...userEntity } = UserEntity.fromObject(user);
       return {
-        user: { ...userEntity },
+        user: userEntity,
         token: 'ABD',
       };
     } catch (error) {
       throw CustomError.internalServer(`${error}`);
+    }
+  }
+
+  public async loginUser(loginUserDto: LoginUserDto) {
+    const user = await UserModel.findOne({ email: loginUserDto.email });
+    if (!user) throw CustomError.badRequest('Email not exist');
+
+    try {
+      const hasMatch = bcryptAdapter.compare(loginUserDto.password, user.password);
+      if (!hasMatch) throw CustomError.badRequest('Credentials are wrong');
+      const { password, ...userEntity } = UserEntity.fromObject(user);
+      return {
+        user: userEntity,
+        token: 'ABC',
+      }
+    } catch (error) {
+      throw CustomError.internalServer(`${error}`)
     }
   }
 };
