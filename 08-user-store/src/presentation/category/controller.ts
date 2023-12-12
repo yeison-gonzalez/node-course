33f6468d154ff97cf594e596a4ff202378bdd1e5
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-12-12 15:36:29 -0500

    [ADD] create pagination dto

--- 08-user-store/src/presentation/category/controller.ts
+++ 08-user-store/src/presentation/category/controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express'
 import { CreateCategoryDto, CustomError } from '../../domain';
-import { AuthService } from '../services/auth.service';
 import { CategoryService } from '../services';
 
 export class CategoryController {
