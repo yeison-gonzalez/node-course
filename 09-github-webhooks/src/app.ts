f184a9854adedcf8fc3d73bf0fbfd20094aed17c
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-12-19 15:49:49 -0500

    [ADD] create controller github

--- 09-github-webhooks/src/app.ts
+++ 09-github-webhooks/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { envs } from './config';
+import { GithubController } from './presentation/github/controller';
 
 (() => {
   main();
@@ -7,9 +8,9 @@ import { envs } from './config';
 
 function main() {
   const app = express();
+  const controller = new GithubController();
 
-  app.post('/api/github', (req, res) => {
-    res.json('Github Endpoint');
-  })
+  app.post('/api/github', controller.webhookHandler);
+
   app.listen(envs.PORT, () => console.log(`Server running http://localhost:${envs.PORT}`));
 }
