f4d2fe62cee8d38bcd077e9259d8b9e4472249cb
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2024-01-02 14:52:25 -0500

    [ADD] create hello world in netlify

---
+++ 10-serverless/netlify/functions/hello/hello.ts
@@ -0,0 +1,15 @@
+import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hola mundo'
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+}
+
+export { handler };
