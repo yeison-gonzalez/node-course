ae9f0c22185c14d8840224c8f19687fa94be59cd
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2024-01-04 14:47:39 -0500

    [ADD] add server broadcast

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -8,12 +8,18 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    const payload = {
+    
+
+    const payload = JSON.stringify({
       type: 'custom-message',
       payload: data.toString(),
-    }
-    ws.send(JSON.stringify(payload));
+    });
+    // ws.send(JSON.stringify(payload));
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
   });
 
   ws.on('close', () => {
