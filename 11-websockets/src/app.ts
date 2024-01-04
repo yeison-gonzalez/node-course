b3d50bb814baa51294e50cbab2daa7632cb99cb9
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2024-01-04 14:50:52 -0500

    [ADD] add specific broadcast

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -8,15 +8,20 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    
-
     const payload = JSON.stringify({
       type: 'custom-message',
       payload: data.toString(),
     });
     // ws.send(JSON.stringify(payload));
-    wss.clients.forEach(function each(client) {
+    //* Todos - incluyente
+    /* wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    }); */
+    //* Todos - excluyente
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(payload, { binary: false });
       }
     });
