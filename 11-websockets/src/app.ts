ea68ba86d468c4c195755779ebc40e2a40494689
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2024-01-04 14:18:06 -0500

    [ADD] basic example ws server

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hola mundo')
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+  
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('Hello from server');
+});
+
+console.log('Server running on port http://localhost:3000');
