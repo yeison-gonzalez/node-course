2891aaf1c6e0cbf6fa58448faf58e36ccf5e450e
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2024-01-05 15:33:29 -0500

    [ADD] connect functionalities with controller

--- 13-socket-server/src/presentation/services/ticket.service.ts
+++ 13-socket-server/src/presentation/services/ticket.service.ts
@@ -2,7 +2,7 @@ import { UuidAdapter } from "../../config/uuid.adapter";
 import { Ticket } from "../../domain/interfaces/ticket";
 
 export class TicketService {
-  private readonly tickets: Ticket[] = [
+  public readonly tickets: Ticket[] = [
     { id: UuidAdapter.v4(), number: 1, createdAt: new Date(), done: false },
     { id: UuidAdapter.v4(), number: 2, createdAt: new Date(), done: false },
     { id: UuidAdapter.v4(), number: 3, createdAt: new Date(), done: false },
@@ -11,18 +11,24 @@ export class TicketService {
     { id: UuidAdapter.v4(), number: 6, createdAt: new Date(), done: false },
   ];
 
+  private readonly workingOnTickets: Ticket[] = [];
+
   public get pendingTickets(): Ticket[] {
     return this.tickets.filter(ticket => !ticket.handleAtDesk);
   }
 
-  public lastTicketNumber() {
+  public get lastWorkingOnTickets(): Ticket[] {
+    return this.workingOnTickets.splice(0, 4);
+  }
+
+  public get lastTicketNumber() {
     return this.tickets.length > 0 ? this.tickets.at(-1)!.number : 0;
   }
 
   public createTicket() {
     const ticket: Ticket = {
       id: UuidAdapter.v4(),
-      number: this.lastTicketNumber() + 1,
+      number: this.lastTicketNumber + 1,
       createdAt: new Date(),
       done: false,
       handleAt: undefined,
@@ -41,6 +47,22 @@ export class TicketService {
     ticket.handleAtDesk = desk;
     ticket.handleAt = new Date();
 
+    this.workingOnTickets.unshift({ ...ticket });
+
     return { status: 'ok', ticket };
+  }
+
+  public onFinishedTicket(id: string) {
+    const ticket = this.tickets.find(t => t.id === id);
+    if (!ticket) return { status: 'error', message: 'Ticket no encontrado' };
+  
+    this.tickets.map(ticket => {
+      if (ticket.id === id) {
+        ticket.done = true;
+      }
+      return ticket;
+    });
+
+    return { status: 'ok' };
   }
 }
