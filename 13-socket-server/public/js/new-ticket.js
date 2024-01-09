724e1d4115b7e77e46fc5b5060b22e79b4375b2d
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2024-01-09 16:07:26 -0500

    [ADD] add functionality to add ticket in front end

--- 13-socket-server/public/js/new-ticket.js
+++ 13-socket-server/public/js/new-ticket.js
@@ -1,1 +1,19 @@
-console.log('Nuevo Ticket HTML');
+const currentTicketLbl = document.querySelector('span');
+const createTicketBtn = document.querySelector('button');
+
+async function getLastTicket() {
+  const lastTicket = await fetch('/api/ticket/last').then(res => res.json());
+  currentTicketLbl.innerText = lastTicket;
+}
+
+async function createTicket() {
+  const newTicket = await fetch('/api/ticket', {
+    method: 'POST'
+  }).then(res => res.json());
+
+  currentTicketLbl.innerText = newTicket.number;
+}
+
+createTicketBtn.addEventListener('click', createTicket);
+
+getLastTicket();
