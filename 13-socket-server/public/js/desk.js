a0e7834b5634c9b2bdffdbc9cd88ad59aaefe453
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2024-01-09 16:31:28 -0500

    [ADD] remove alert

--- 13-socket-server/public/js/desk.js
+++ 13-socket-server/public/js/desk.js
@@ -1,8 +1,28 @@
 const lblPending = document.querySelector('#lbl-pending');
+const deskHeader = document.querySelector('h1');
+const noMoreAlert = document.querySelector('.alert');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+  window.location = 'index.html';
+  throw new Error('Escritorio es requerido');
+}
+const deskNumber = searchParams.get('escritorio')
+deskHeader.innerText = deskNumber;
+
+function checkTicketCount(currentCount = 0) {
+  if (currentCount === 0) {
+    noMoreAlert.classList.remove('d-none');
+  } else {
+    noMoreAlert.classList.add('d-none');
+  }
+  lblPending.innerHTML = currentCount;
+}
 
 async function loadInitialCount() {
   const pending = await fetch('/api/ticket/pending').then(res => res.json());
-  lblPending.innerHTML = pending.length || 0;
+  checkTicketCount(pending.length);
 }
 
 function connectToWebSockets() {
@@ -11,7 +31,7 @@ function connectToWebSockets() {
   socket.onmessage = (event) => {
     const { type, payload } = JSON.parse(event.data);
     if (type !== 'on-ticket-count-changed') return;
-    lblPending.innerHTML = payload;
+    checkTicketCount(payload)
   };
 
   socket.onclose = ( event ) => {
